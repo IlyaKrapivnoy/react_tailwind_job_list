7a9b09de83c04405a19ddf6c3c01a71dd546e488
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-10-29 22:23:34 +0300

    B01/feature useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import ListingsData from './data/data.json';
 
 function App() {
     const [listings, setListings] = useState([]);
+    useEffect(() => {
+        setListings(ListingsData);
+    }, []);
+    console.log('listings', listings);
+
     return <div className='App'>Hello World</div>;
 }
 
