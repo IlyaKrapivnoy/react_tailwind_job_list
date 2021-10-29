18b6d23798a44d68100e79532b393e8b38db77e2
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-10-29 22:20:06 +0300

    B01/feature add data and useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './App.css';
+import ListingsData from './data/data.json';
 
 function App() {
+    const [listings, setListings] = useState([]);
     return <div className='App'>Hello World</div>;
 }
 
