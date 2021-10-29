01466bdcb4c8958a210251cdf5aa88b769011c21
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-10-29 22:48:45 +0300

    B01/feature add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,15 @@
+import HeaderImg from '../images/bg-header-desktop.svg';
+
 const Header = () => {
-    return <div></div>;
+    return (
+        <div className='py-4 relative h-32'>
+            <img
+                src={HeaderImg}
+                className='absolute inset-0 w-full object-cover'
+                // alt=''
+            />
+        </div>
+    );
 };
 
 export default Header;
