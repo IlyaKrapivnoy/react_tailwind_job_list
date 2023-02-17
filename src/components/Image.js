eb4fc64784a82036899eea2207dc2f7d6d21026f
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-02-17 17:43:06 +0100

    change Details component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,7 @@
-const Image = ({ url, desc = '' }) => {
-    return (
-        <div className='flex-2 p-2'>
-            <img src={url} alt={desc} />
-        </div>
-    );
-};
+const Image = ({ url, desc = '' }) => (
+    <div className='flex-2 p-2'>
+        <img src={url} alt={desc} />
+    </div>
+);
 
 export default Image;
